4d062be6cceab57d5156cb90e3eee20750d215a3
dev juninho <rodrigoduarte9761@gmail.com>
2026-05-10 01:50:57 -0300

    feat: add official teams and player names

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'figurinhas-copa-2026-v1';
+const CACHE = 'figurinhas-copa-2026-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (e) => {
