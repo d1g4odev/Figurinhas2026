4d00e1497ca84eb814715ea16b0aef55fa87bac2
d1g40dev <contato.rodrigoduartemkt@gmail.com>
2026-05-10 22:14:48 -0300

    chore: switch firebase project

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDSw00VgqOxULKNYRqGQ6fdZumXKWsd19Q',
-  authDomain: 'figurinhas2026-9bd5b.firebaseapp.com',
-  projectId: 'figurinhas2026-9bd5b',
-  storageBucket: 'figurinhas2026-9bd5b.firebasestorage.app',
-  messagingSenderId: '452659666470',
-  appId: '1:452659666470:web:8b411bb9e3f5e6d7ea10e7',
-  measurementId: 'G-0YW5HPKYHT'
+  apiKey: 'AIzaSyAPSLXcMmIsgEMP1blvT_U3ge3fI6zMlj8',
+  authDomain: 'figs2026.firebaseapp.com',
+  projectId: 'figs2026',
+  storageBucket: 'figs2026.firebasestorage.app',
+  messagingSenderId: '529737939476',
+  appId: '1:529737939476:web:443c1e3a33955513b04501',
+  measurementId: 'G-07PVZRX3RY'
 };
